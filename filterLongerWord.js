9c3405ea70ef4d886891013491e8dab4e631ed9e
MikeAttia <80725527+MikeAttia@users.noreply.github.com>
2021-04-15 18:58:17 -0400

    Add files via upload

---
+++ filterLongerWord.js
@@ -0,0 +1,15 @@
+function findLongestWord(str)
+{
+  var array1 = str.match(/\w[a-z]{0,}/gi);
+  var result = array1[0];
+
+  for(var i = 1 ; i < array1.length ; i++)
+  {
+    if(result.length < array1[i].length)
+    {
+    result = array1[i];
+    } 
+  }
+  return result;
+}
+console.log(findLongestWord('filter longest word exercise'));
